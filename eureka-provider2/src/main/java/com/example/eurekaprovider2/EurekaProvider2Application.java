0f8dcf834e09a6c59afa0de5ba18619dbffc549d
zeng-s-h <zengsh625362190@163.com>
2020-07-20 10:15:19 +0800

    Eureka服务提供者2

---
+++ eureka-provider2/src/main/java/com/example/eurekaprovider2/EurekaProvider2Application.java
@@ -0,0 +1,44 @@
+package com.example.eurekaprovider2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaProvider2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaProvider2Application.class, args);
+    }
+
+    /**
+     * 假如这个客户端要提供一个getUser的方法
+     *
+     * @return Map<String, Object>
+     */
+    @GetMapping(value = "/getUser1")
+    @ResponseBody
+    public Map<String, Object> getUser(@RequestParam Integer id) {
+
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("id", id);
+
+        data.put("userName", "admin");
+        //改这里是为了让大家更能理解它负载均衡的机制
+        data.put("from", "provider-B");
+
+        return data;
+
+    }
+
+}
