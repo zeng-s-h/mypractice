62b8a79d75ff6ca25052029a75dea6ca36374de8
zeng-s-h <zengsh625362190@163.com>
2020-07-23 17:15:34 +0800

    添加author注释

--- login-session/src/main/java/com/example/loginsession/LoginSessionApplication.java
+++ login-session/src/main/java/com/example/loginsession/LoginSessionApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@MapperScan("com.example.loginsession") //扫描的mapper
+/**
+ * @author 小白i
+ */
+
+@MapperScan("com.example.loginsession")
 @SpringBootApplication
 public class LoginSessionApplication {
 
