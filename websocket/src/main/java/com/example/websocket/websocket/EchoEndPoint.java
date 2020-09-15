e2d4ac6a522895e250344581e0afbe61f6bc6568
zeng-s-h <zengsh625362190@163.com>
2020-09-15 14:03:36 +0800

    websocket

---
+++ websocket/src/main/java/com/example/websocket/websocket/EchoEndPoint.java
@@ -0,0 +1,41 @@
+package com.example.websocket.websocket;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+/**
+ * @author 小白i
+ * @date 2020/7/27
+ */
+@ServerEndpoint("/push")
+public class EchoEndPoint {
+
+    /**
+     * 网络连接建立时触发
+     *
+     * @param session session会话
+     * @throws IOException 异常
+     */
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        //以下代码省略...
+    }
+
+    @OnMessage
+    public String onMessage(String message) {
+        //以下代码省略...
+        return "message";
+    }
+
+    @OnError
+    public void onError(Throwable t) {
+        //以下代码省略...
+    }
+
+    @OnClose
+    public void onClose(Session session, CloseReason reason) {
+        //以下代码省略...
+    }
+
+}
